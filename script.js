171c001d673f200137fc6cb2d7a9d5f834e3a941
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-03-01 12:38:56 +0700

    Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const pos = `https://www.google.com/maps/place/10%C2%B050'30.7%22N+106%C2%B040'51.1%22E/@${latitude},${longitude}`;
+      console.log(pos);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
