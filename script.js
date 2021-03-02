cb5db98deaaeba81c2da45f68ff33d5815cece5a
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-03-02 11:36:19 +0700

    Create classes

--- script.js
+++ script.js
@@ -11,7 +11,50 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+}
+  
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/hour
+    this.speed = this.distance / (this.duration/60)
+    return this.speed
+  }
+}
+
+// const run1 = new Running([39,-12], 5.2, 24, 178)
+// const cycling1 = new Cycling([39,-12], 27, 95, 523)
+// console.log(run1, cycling1);
+
+// ////////////////////////
+// APPLICATION ARCHITECTURE
+// ////////////////////////
 class App {
   #map;
   #mapEvent;
@@ -38,7 +81,6 @@ class App {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
-    console.log(this);
 
     this.#map = L.map('map').setView(coords, 13);
 
